49191af873950f32df9d38c931cd54cdb83d8f7c
ArvidW <71834553+ArvidWedtstein@users.noreply.github.com>
2022-01-28 09:29:28 +0100

    d

--- index.js
+++ index.js
@@ -22,18 +22,18 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }))
 app.use('/uploads', express.static('uploads'));
 
-var allowlist = ['https://nuxt.arvidw.space', 'https://arvidw.space', 'https://nuxt.arvidw.space/auth/register', "http://localhost:3000"]
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
-app.use(cors(corsOptionsDelegate));
-app.use((req, res, next) => {
+// var allowlist = ['https://arvidw.space', "http://localhost:3000"]
+// var corsOptionsDelegate = function (req, callback) {
+//   var corsOptions;
+//   if (allowlist.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false } // disable CORS for this request
+//   }
+//   callback(null, corsOptions) // callback expects two parameters: error and options
+// }
+// app.use(cors(corsOptionsDelegate));
+/* app.use((req, res, next) => {
   // res.setHeader("Access-Control-Allow-Origin", 'https://nuxt.arvidw.space')
   res.setHeader("Access-Control-Allow-Origin", 'https://nuxtarvidw.netlify.app')
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
@@ -42,9 +42,9 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  // res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
-});
+});*/
 //app.use(authenticateMiddleware);
 // Router //
 app.use("/api/auth", authRouter);
